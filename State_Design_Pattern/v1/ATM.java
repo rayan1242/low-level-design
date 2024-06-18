2f3ae35157efed987031b40275d8d15372b73603
rayan1242 <rayyanmaindargi12@gmail.com>
2024-06-18 19:52:33 +0530

    added code for v1

--- State_Design_Pattern/v1/ATM.java
+++ State_Design_Pattern/v1/ATM.java
@@ -1,3 +1,4 @@
+//OCP AND ISP is violated
 package State_Design_Pattern.v1;
 
 import v1.ATMSTATES;
@@ -16,8 +17,44 @@ public class ATM {
     }
 
     public String init(){
+        if(this.state.equals(ATMSTATES.CARD_READING)){
+            throw new IllegalArgumentException("Txn in process");
+        }
+        if(this.state.equals(ATMSTATES.CARD_EJECTING)){
+            throw new IllegalArgumentException("Txn in process");
+        }
+        if(this.state.equals(ATMSTATES.CASH_WITHDRAWAL)){
+            throw new IllegalArgumentException("Txn in process");
+        }
         String txn=UUID.randomUUID.toString();
         this.state=ATMSTATES.CASH_READING;
         return txn;
+    }
+    
+    public void cancelTxn(){
+        if(this.state.equals(ATMSTATES.CARD_READING)){
+        }
+        if(this.state.equals(ATMSTATES.CARD_EJECTING)){
+        }
+        if(this.state.equals(ATMSTATES.CASH_WITHDRAWAL)){
+        }
+    }
+
+    public void readCard(){
+        if(this.state.equals(ATMSTATES.CARD_READING)){
+        }
+        if(this.state.equals(ATMSTATES.CARD_EJECTING)){
+        }
+        if(this.state.equals(ATMSTATES.CASH_WITHDRAWAL)){
+        }
+    }
+
+    public void withdrawCash(){
+        if(this.state.equals(ATMSTATES.CARD_READING)){
+        }
+        if(this.state.equals(ATMSTATES.CARD_EJECTING)){
+        }
+        if(this.state.equals(ATMSTATES.CASH_WITHDRAWAL)){
+        }
     }
 }
